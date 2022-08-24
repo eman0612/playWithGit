257788ab9cc2c47a017621dc065f26e4a3713ad0
Emanuel Sobral <eman0612@stud.kea.dk>
2022-08-24 12:39:44 +0200

    maintain css and js

---
+++ main.js
@@ -0,0 +1,27 @@
+const firstName = "Emanuel";
+const lastName = "Sobral";
+
+const name = firstName + lastName;
+
+function addFirstLastName(firstName, lastName) {
+  return firstName + lastName;
+}
+
+const firstNames = ["Emanuel", "Nemi"];
+const lastNames = ["Sobral", "Thapa"];
+
+const person1 = {
+  firstName: "Emanuel",
+  lastName: "Sobral",
+  age: 28,
+};
+
+const person2 = {
+  firstName: "Nemi",
+  lastName: "Thapa",
+  age: 27,
+};
+
+function getAge(person) {
+  return person.age;
+}
